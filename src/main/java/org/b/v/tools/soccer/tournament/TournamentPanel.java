0e9316185359f6ca433d6797248325d20ae95778
Bart Voet <bart@MacBook-Pro-van-Bart.local>
2016-03-06 20:09:51 +0100

    [initial,poc] Adding a second table to the main screen

--- src/main/java/org/b/v/tools/soccer/tournament/TournamentPanel.java
+++ src/main/java/org/b/v/tools/soccer/tournament/TournamentPanel.java
@@ -1,7 +1,10 @@
 package org.b.v.tools.soccer.tournament;
 
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
@@ -19,21 +22,33 @@ public class TournamentPanel extends JPanel {
 	public DefaultTableModel model;
 	
     public TournamentPanel() {
-        super(new GridLayout(1,0));
- 
+        //super(new GridLayout(1,0));
+    	super(new BorderLayout());
+    	GridBagConstraints c = new GridBagConstraints();
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	
+    	
         String[] columnNames = {"Team","Team","Groep","Veld","Tijdstip"};
- 
         Object[][] data = {};
- 
         model=new DefaultTableModel(data, columnNames);
         
         final JTable table = new JTable(model);
-
-        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setPreferredScrollableViewportSize(new Dimension(500, 200));
         table.setFillsViewportHeight(true);
- 
         JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane);
+        
+        
+        String[] groupColumnNames = {"Groep","Team","Punten"};
+        Object[][] groupEata = {{"U17","Baal","10"}};
+        
+        DefaultTableModel groupModel=new DefaultTableModel(groupEata, groupColumnNames);
+        final JTable groupTable = new JTable(groupModel);   
+        groupTable.setPreferredScrollableViewportSize(new Dimension(500, 200));
+        groupTable.setFillsViewportHeight(true);
+        JScrollPane datascrollPane = new JScrollPane(groupTable);
+        
+        add(scrollPane,BorderLayout.CENTER);
+        add(datascrollPane,BorderLayout.EAST);
     }
  
 }
