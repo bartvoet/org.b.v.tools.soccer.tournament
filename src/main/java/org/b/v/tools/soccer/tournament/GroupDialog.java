793834a1f9ee2954e97329415c3fbfcf26742a80
bart <bart_voet@telenet.be>
2016-03-27 10:27:38 +0200

    [cleanup,imports] Cleanup overhead imports

--- src/main/java/org/b/v/tools/soccer/tournament/GroupDialog.java
+++ src/main/java/org/b/v/tools/soccer/tournament/GroupDialog.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
-import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
