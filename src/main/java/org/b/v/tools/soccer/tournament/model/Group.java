56c4cd3fb388c0c418e7b5d53392cebda98aaf46
bart <bart_voet@telenet.be>
2016-04-04 20:08:34 +0200

    [cleanup] removing imports

--- src/main/java/org/b/v/tools/soccer/tournament/model/Group.java
+++ src/main/java/org/b/v/tools/soccer/tournament/model/Group.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 import org.b.v.tools.soccer.tournament.extra.Entity;
 
