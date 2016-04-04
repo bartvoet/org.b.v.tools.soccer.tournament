86eb78fd0be8679a77902bba529b0e8c09256601
bart <bart_voet@telenet.be>
2016-04-04 19:47:23 +0200

    [cleanup] removing imports

--- src/main/java/org/b/v/tools/soccer/tournament/TournamentApplication.java
+++ src/main/java/org/b/v/tools/soccer/tournament/TournamentApplication.java
@@ -2,7 +2,6 @@ package org.b.v.tools.soccer.tournament;
 
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -10,7 +9,6 @@ import java.awt.event.KeyEvent;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
