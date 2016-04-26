4602c75ae889db2d3112ddcba8ac8b9efb05a217
bart <bart_voet@telenet.be>
2016-04-26 15:15:33 +0200

    [model] Adding a category

---
+++ src/main/java/org/b/v/tools/soccer/tournament/model/Category.java
@@ -0,0 +1,18 @@
+package org.b.v.tools.soccer.tournament.model;
+
+import org.b.v.tools.soccer.tournament.extra.Entity;
+
+public class Category extends Entity {
+	
+	private static final long serialVersionUID = 1L;
+	private String name;
+
+	public Category(String name) {
+		this.name=name;
+	}
+	
+	public String name() {
+		return this.name;
+	}
+
+}
