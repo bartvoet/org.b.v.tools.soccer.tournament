028f3e049b46ac9780d2392bb64223a8bd537ba9
bart <bart_voet@telenet.be>
2016-04-26 11:34:57 +0200

    [cleanup] Cleaning up imports

--- src/main/java/org/b/v/tools/soccer/tournament/TournamentApplication.java
+++ src/main/java/org/b/v/tools/soccer/tournament/TournamentApplication.java
@@ -2,22 +2,17 @@ package org.b.v.tools.soccer.tournament;
 
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -76,16 +71,7 @@ public class TournamentApplication extends JFrame implements UpdateEvent {
 			}
 		});
 
-
-        JMenuItem addMatch = new JMenuItem("Voeg wedstrijd toe");
-        addMatch.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-        });
-
-        
+  
         JMenuItem addMatchVsMatch = new JMenuItem("Match tegen match");
         addMatchVsMatch.addActionListener(new ActionListener() {
 			@Override
@@ -104,7 +90,6 @@ public class TournamentApplication extends JFrame implements UpdateEvent {
         
         menu.add(addGroups);
         menu.add(addTerrain);
-        menu.add(addMatch);
         menu.add(addGroupVsGroup);
         menuBar.add(menu);
 	}
